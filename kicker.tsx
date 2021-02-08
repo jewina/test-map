b55a0eca1018828547dfb199b9450a8f8c7b55ed
jewina <j.winzer1green@gmail.com>
2021-02-08 10:08:23 +0100

    Create kicker.tsx

---
+++ kicker.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="kicker" tilewidth="32" tileheight="32" tilecount="12" columns="4">
+ <image source="../tiles doko/doko.png" trans="005500" width="135" height="116"/>
+</tileset>
